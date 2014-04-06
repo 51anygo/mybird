8b6e06effc58027cb8fc5b3dc131deb6ff1f3999
51anygo <51anygo@gmail.com>
2014-04-06 14:27:12 +0800

    gitignore added new

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /cygdrive/d/workspace/game/cocos2d-x-2.2.2/projects/mybird/proj.android/../../../cocos2dx/support/ccUtils.cpp \
- /cygdrive/d/workspace/game/cocos2d-x-2.2.2/projects/mybird/proj.android/../../../cocos2dx/support/ccUtils.h
-
-/cygdrive/d/workspace/game/cocos2d-x-2.2.2/projects/mybird/proj.android/../../../cocos2dx/support/ccUtils.h:
